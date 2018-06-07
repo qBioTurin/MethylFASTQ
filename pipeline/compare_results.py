142183e55d1614ec0bb3dd84d5abe5843e929bc9
Cursecatcher <nicola.licheri@gmail.com>
2018-06-07 22:35:48 +0200

    compare_results works with both .bed and .ch3 file

--- pipeline/compare_results.py
+++ pipeline/compare_results.py
@@ -31,35 +31,33 @@ def compare(gold_map, tool_map):
 
     return tp, cm
 
+def load_gold_file(filename):
+    ext = filename.split(".")[-1].lower()
+    cpg = set()
 
-if __name__ == "__main__":
-    if len(sys.argv) < 5:
-        sys.exit("Usage: {} cg_gold_file methratio_file bismark_file logfile".format(sys.argv[0]))
+    with open(filename) as f:
+        csvfile = csv.reader(f, delimiter="\t")
 
-    gold = sys.argv[1]
-    methratio_output = sys.argv[2]
-    bismark_meth = sys.argv[3]
-    logfile = open(sys.argv[4], "w")
+        if ext == "bed":
+            for row in csvfile:
+                ch, pos = row[:2]
+                cytosine = ch, int(pos)
+                cpg.add(cytosine)
+        elif ext == "ch3":
+            for row in csvfile:
+                if row[3] == "CytosineContext.CG":
+                    cytosine = (row[0], int(row[1]) + 1)
+                    cpg.add(cytosine)
+        else:
+            sys.exit("Unexpected input file")
 
-    cpg_map = set()
-    bsmap_map = set()
-    bismark_map = set()
+    return cpg
 
-    logfile.write("Reading CpG file {}...\n".format(gold))
+def load_bsmap_methylation(filename):
+    cpg = set()
 
-    #legge file .ch3
-    with open(gold) as handle:
-        csvfile = csv.reader(handle, delimiter="\t")
-        for row in csvfile:
-            if row[3] == "CytosineContext.CG":
-                cytosine = (row[0], int(row[1]) + 1)
-                cpg_map.add(cytosine)
-
-    logfile.write("Analysing methratio file {}...\n".format(methratio_output))
-
-    #legge output methratio
-    with open(methratio_output) as handle:
-        csvfile = csv.reader(handle, delimiter="\t")
+    with open(filename) as f:
+        csvfile = csv.reader(f, delimiter="\t")
         next(csvfile, None) #skip header
 
         for row in csvfile:
@@ -67,23 +65,81 @@ if __name__ == "__main__":
 
             if context == "CG":
                 cytosine = ch, int(pos)
-                bsmap_map.add(cytosine)
+                cpg.add(cytosine)
+
+    return cpg
+
+def load_bismark_methylation(filename):
+    cpg = set()
+
+    with open(filename) as f:
+        for row in csv.reader(f, delimiter="\t"):
+            ch, pos = row[2:4]
+            cytosine = ch, int(pos)
+            cpg.add(cytosine)
+
+    return cpg
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="methyl-fastq")
+
+    parser.add_argument("--gold", dest="gold_file", metavar="cg_gold_file", action="store",
+                        type=str, help=".bed for real data or .ch3 for synthetic data")
+    # parser.add_argument("--ch3", dest="ch3_file", metavar="ch_gold_file.ch3", action="store";
+    #                     type=str, help="")
+    parser.add_argument("--bsmap"; dest="bsmap_file", metavar="methratio_output_file",
+                        action="store", type=str, help="")
+    parser.add_argument("--bismark"; dest="bismark_file", metavar="bismark_methylation_extractor_output_file",
+                        action="store", type=str, help="")
+    parser.add_argument("--log", dest="logfile", metavar="log file", action="store", type=str, help="")
+
+
+    logfile = open(parser.logfile, "w")
+
+    logfile.write("Reading CpG file {}...\n".format(parser.gold_file))
+    cpg_gold = load_gold_file(parser.gold_file)
+
+
+    # #legge file .ch3
+    # with open(gold) as handle:
+    #     csvfile = csv.reader(handle, delimiter="\t")
+    #     for row in csvfile:
+    #         if row[3] == "CytosineContext.CG":
+    #             cytosine = (row[0], int(row[1]) + 1)
+    #             cpg_map.add(cytosine)
+
+    logfile.write("Analysing methratio file {}...\n".format(parser.bsmap_file))
+    bsmap_map = load_bsmap_methylation(parser.bsmap_file)
+
+    #legge output methratio
+    # with open(methratio_output) as handle:
+    #     csvfile = csv.reader(handle, delimiter="\t")
+    #     next(csvfile, None) #skip header
+    #
+    #     for row in csvfile:
+    #         ch, pos, strand, context = row[:4]
+    #
+    #         if context == "CG":
+    #             cytosine = ch, int(pos)
+    #             bsmap_map.add(cytosine)
 
     tp_bsmap, bsmap_confusion_matrix = compare(cpg_map, bsmap_map)
     print_confusion_matrix(logfile, "bsmap", bsmap_confusion_matrix)
 
-    logfile.write("Analysing bismark methylation file {}...\n".format(bismark_meth))
+    logfile.write("Analysing bismark methylation file {}...\n".format(parser.bismark_file))
+    bismark_map = load_bismark_methylation(parser.bismark_file)
 
 
     #legge output bismark.methylation_extractor - file .Cpg_report.txt
-    with open(bismark_meth) as handle:
-        csvfile = csv.reader(handle, delimiter="\t")
-    #    next(csvfile, None)
-
-        for row in csvfile:
-            ch, pos = row[: 2]
-            cytosine = ch, int(pos)
-            bismark_map.add(cytosine)
+    # with open(bismark_meth) as handle:
+    #     csvfile = csv.reader(handle, delimiter="\t")
+    # #    next(csvfile, None)
+    #
+    #     for row in csvfile:
+    #         ch, pos = row[: 2]
+    #         cytosine = ch, int(pos)
+    #         bismark_map.add(cytosine)
 
     tp_bismark, bismark_confusion_matrix = compare(cpg_map, bismark_map)
     print_confusion_matrix(logfile, "bismark", bismark_confusion_matrix)
